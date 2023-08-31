f3a56211b5fe049f31ba33f8cfa0f143544c888c
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-08-31 06:09:15 +0000

    [Fix]部分テンプレートrender内の記述変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,10 +2,14 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+    @book_new = Book.new
   end
 
   def index
+    @book = Book.new
     @books = Book.all
+    @user = current_user
   end
 
   def create
@@ -15,7 +19,7 @@ class BooksController < ApplicationController
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
-      render 'index'
+      render :index
     end
   end
 
@@ -41,6 +45,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 end
