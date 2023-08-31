24e8641a183a2ec758fd2b9cb29dc12bd0304e13
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-08-31 10:44:55 +0000

    [Add]RSpecエラー解消

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,18 +19,25 @@ class BooksController < ApplicationController
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
+      @user = current_user
       render :index
     end
   end
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+      render :edit
+    else
+      redirect_to books_path
+    end
   end
 
   def update
     @book = Book.find(params[:id])
+    @book.user_id == current_user.id
     if @book.update(book_params)
-      redirect_to book_path(@book), notice: "You have updated book successfully."
+      redirect_to book_path(@book.id), notice: "You have updated book successfully."
     else
       render "edit"
     end
@@ -47,4 +54,5 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+  
 end
