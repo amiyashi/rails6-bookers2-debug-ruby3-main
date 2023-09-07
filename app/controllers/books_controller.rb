597f99d9c2c549cc790ad4537d582090039a2e83
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-09-07 07:56:42 +0000

    [Add]閲覧数カウント➝表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,10 @@ class BooksController < ApplicationController
     @user = @book.user
     @book_new = Book.new
     @post_comment = PostComment.new
+    @room = Room.new
+    unless ReadCount.where(created_at: Time.zone.now.all_day).find_by(user_id: current_user.id, book_id: @book.id)
+      current_user.read_counts.create(book_id: @book.id)
+    end
   end
 
   def index
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,25 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:edit, :update]
+  before_action :ensure_correct_user, only: [:show, :edit, :update]
 
   def show
     @user = User.find(params[:id])
+    @currentUserEntry=Entry.where(user_id: current_user.id)
+    @anotherUserEntry=Entry.where(user_id: @user.id)
+    unless @user.id == current_user.id
+      @currentUserEntry.each do |current|
+        @anotherUserEntry.each do |another|
+          if current.room_id == another.room_id then
+            @is_room = true
+            @room_id = current.room_id
+          end
+        end
+      end
+      if @is_room
+      else
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
     @books = @user.books
     @book = Book.new
   end
