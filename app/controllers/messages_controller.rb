499c0497915675f6e7826fc9c717dc2eac3a5a09
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-09-08 02:23:46 +0000

    [Add]messagesコントローラのコメントアウト

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,35 +1,31 @@
 class MessagesController < ApplicationController
-  #showページにアクセスしたときのみfollowing_check
-  before_action :following_check, only: [:show]
   
-  def show
-    @user = User.find(params[:id])
-    @message = Message.new
-    @messages = Message.where(send_user_id: current_user.id,receive_user_id: params[:id]).or(@receive_messages = Message.where(send_user_id: params[:id],receive_user_id: current_user.id)).order(:created_at)
-  end
+  # def show
+  #   @user = User.find(params[:id])
+  #   @message = Message.new
+  #   @messages = Message.where(send_user_id: current_user.id,receive_user_id: params[:id]).or(@receive_messages = Message.where(send_user_id: params[:id],receive_user_id: current_user.id)).order(:created_at)
+  # end
     
   def create
-    @message = Message.new(message_params)
-    @message.send_user_id = current_user.id
-    if @message.save!
-      @messages = Message.where(send_user_id: current_user.id,receive_user_id: params[:message][:receive_user_id]).or(@receive_messages = Message.where(send_user_id: params[:message][:receive_user_id], receive_user_id: current_user.id)).order(:created_at)
+    if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present? #user_idがcurrent_user.idで、room_idがparams[:message]のroom_idであるEntryが存在するなら
+      @message = Message.new(message_params)
+      @message.user_id = current_user.id
+      @message.save
     else
-      @message = Message.new
-      @messages = Message.where(send_user_id: current_user.id,receive_user_id: params[:message][:receive_user_id]).or(@receive_messages = Message.where(send_user_id: params[:message][:receive_user_id], receive_user_id: current_user.id)).order(:created_at)
-      render :message
+      flash[:alert] = "メッセージ送信に失敗しました。"
     end
   end
 
   private
   def message_params
-    params.require(:message).permit(:receive_user_id, :message)
+    params.require(:message).permit(:user_id, :room_id, :message)
   end
   
-  def following_check
-    user = User.find(params[:id])
-    unless current_user.following?(user) && user.following?(current_user)
-      redirect_to books_path
-    end
-  end
+  # def following_check
+  #   user = User.find(params[:id])
+  #   unless current_user.following?(user) && user.following?(current_user)
+  #     redirect_to books_path
+  #   end
+  # end
   
 end
